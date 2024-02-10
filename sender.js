903559fa85a87f75cf880d67e0a293b46b2c7a6f
idiotthatcodes <117654244+idiotthatcodes@users.noreply.github.com>
2024-02-10 15:23:03 +0100

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,10 +1,13 @@
 // Default value for belOptie
 var belOptie = "Optie 1, Best";
-
+var locatie;
+var mail = `${locatie.toLowerCase()}@maastelecom.nl`
 function updateContextMenuTitle() {
   chrome.contextMenus.update("Note2", {
     title: `Bel Optie: ${belOptie}`,
   });
+
+  
 }
 
 function ExtensionOnClick(info) {
@@ -12,7 +15,7 @@ function ExtensionOnClick(info) {
     const phoneNumber = info.selectionText;
 
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant, \n\n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant, \n\n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
 
     chrome.tabs.create({ url: whatsappURL });
@@ -20,9 +23,9 @@ function ExtensionOnClick(info) {
   if (info.menuItemId === "outofcontract") {
     const phoneNumber = info.selectionText;
 
-    // Beste klant,  \n\n U heeft via onze winkel een abonnement afgesloten op uw mobiele telefoon. Inmiddels is dit abonnement verlopen en is het gekomen in een variabel contract. \nDe prijs van het abonnement blijft hierbij langzaam stijgen. Tevens kunt u hierdoor niet meer kosteloos gebruiken van onze service in de winkel en kan het zijn dat uw telefoon hierdoor gevoelig is geworden voor online fraude. \nOm gebruik te kunnen maken van onze expertise en service in de winkel is het raadzaam om uw telefoon en contract na te laten kijken in onze winkel. \n\nOm verzekerd te blijven van een goede service hebben wij verschillende providers in ons portfolio. \nBij verlenging van uw contract krijgt u gratis voor de duur van uw abonnement ons servicepakket gold. \nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.
+    // Beste klant,  \n\n U heeft via onze winkel een abonnement afgesloten op uw mobiele telefoon. Inmiddels is dit abonnement verlopen en is het gekomen in een variabel contract. \nDe prijs van het abonnement blijft hierbij langzaam stijgen. Tevens kunt u hierdoor niet meer kosteloos gebruiken van onze service in de winkel en kan het zijn dat uw telefoon hierdoor gevoelig is geworden voor online fraude. \nOm gebruik te kunnen maken van onze expertise en service in de winkel is het raadzaam om uw telefoon en contract na te laten kijken in onze winkel. \n\nOm verzekerd te blijven van een goede service hebben wij verschillende providers in ons portfolio. \nBij verlenging van uw contract krijgt u gratis voor de duur van uw abonnement ons servicepakket gold. \nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant,  \n\nU heeft via onze winkel een abonnement afgesloten op uw mobiele telefoon. Inmiddels is dit abonnement verlopen en is het gekomen in een variabel contract. \n\nDe prijs van het abonnement blijft hierbij langzaam stijgen. Tevens kunt u hierdoor niet meer kosteloos gebruiken van onze service in de winkel en kan het zijn dat uw telefoon hierdoor gevoelig is geworden voor online fraude. \n\nOm gebruik te kunnen maken van onze expertise en service in de winkel is het raadzaam om uw telefoon en contract na te laten kijken in onze winkel. \n\nOm verzekerd te blijven van een goede service hebben wij verschillende providers in ons portfolio. \nBij verlenging van uw contract krijgt u gratis voor de duur van uw abonnement ons servicepakket gold. \nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie} \n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant,  \n\nU heeft via onze winkel een abonnement afgesloten op uw mobiele telefoon. Inmiddels is dit abonnement verlopen en is het gekomen in een variabel contract. \n\nDe prijs van het abonnement blijft hierbij langzaam stijgen. Tevens kunt u hierdoor niet meer kosteloos gebruiken van onze service in de winkel en kan het zijn dat uw telefoon hierdoor gevoelig is geworden voor online fraude. \n\nOm gebruik te kunnen maken van onze expertise en service in de winkel is het raadzaam om uw telefoon en contract na te laten kijken in onze winkel. \n\nOm verzekerd te blijven van een goede service hebben wij verschillende providers in ons portfolio. \nBij verlenging van uw contract krijgt u gratis voor de duur van uw abonnement ons servicepakket gold. \nWij nodigen u graag uit in onze winkel. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie} \n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
 
     chrome.tabs.create({ url: whatsappURL });
@@ -30,51 +33,57 @@ function ExtensionOnClick(info) {
   if (info.menuItemId === "kpn_verlengbaar") {
     const phoneNumber = info.selectionText;
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant,\n\nu heeft via onze winkel een KPN abonnement afgesloten dat inmiddels al een tijdje in de verlengperiode zit. Wij zouden u graag even spreken over de tevredenheid met het abonnement, en/of bij tevredenheid het contract weer vast te zetten om u te verzekeren van de beste deal. \nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant,\n\nu heeft via onze winkel een KPN abonnement afgesloten dat inmiddels al een tijdje in de verlengperiode zit. Wij zouden u graag even spreken over de tevredenheid met het abonnement, en/of bij tevredenheid het contract weer vast te zetten om u te verzekeren van de beste deal. \nWij nodigen u graag uit in onze winkel. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
     chrome.tabs.create({ url: whatsappURL });
   }
   if (info.menuItemId === "vodafone_verlengbaar") {
     const phoneNumber = info.selectionText;
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant,\n\nu heeft via onze winkel een Vodafone abonnement afgesloten dat inmiddels al een tijdje in de verlengperiode zit. Wij zouden u graag even spreken over de tevredenheid met het abonnement, en/of bij tevredenheid het contract weer vast te zetten om u te verzekeren van de beste deal. \nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant,\n\nu heeft via onze winkel een Vodafone abonnement afgesloten dat inmiddels al een tijdje in de verlengperiode zit. Wij zouden u graag even spreken over de tevredenheid met het abonnement, en/of bij tevredenheid het contract weer vast te zetten om u te verzekeren van de beste deal. \nWij nodigen u graag uit in onze winkel. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
     chrome.tabs.create({ url: whatsappURL });
   }
   if (info.menuItemId === "ben_verlengbaar") {
     const phoneNumber = info.selectionText;
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant,\n\nu heeft via onze winkel een BEN abonnement afgesloten dat inmiddels al een tijdje in de verlengperiode zit. Wij zouden u graag even spreken over de tevredenheid met het abonnement, en/of bij tevredenheid het contract weer vast te zetten om u te verzekeren van de beste deal. \nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant,\n\nu heeft via onze winkel een BEN abonnement afgesloten dat inmiddels al een tijdje in de verlengperiode zit. Wij zouden u graag even spreken over de tevredenheid met het abonnement, en/of bij tevredenheid het contract weer vast te zetten om u te verzekeren van de beste deal. \nWij nodigen u graag uit in onze winkel. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
     chrome.tabs.create({ url: whatsappURL });
   }
   if (info.menuItemId === "odido_verlengbaar") {
     const phoneNumber = info.selectionText;
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant,\n\nu heeft via onze winkel een Odido abonnement afgesloten dat inmiddels al een tijdje in de verlengperiode zit. Wij zouden u graag even spreken over de tevredenheid met het abonnement, en/of bij tevredenheid het contract weer vast te zetten om u te verzekeren van de beste deal. \nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://https://telecombinatie.nl/afspraak  \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant,\n\nu heeft via onze winkel een Odido abonnement afgesloten dat inmiddels al een tijdje in de verlengperiode zit. Wij zouden u graag even spreken over de tevredenheid met het abonnement, en/of bij tevredenheid het contract weer vast te zetten om u te verzekeren van de beste deal. \nWij nodigen u graag uit in onze winkel. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://https://telecombinatie.nl/afspraak  \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
     chrome.tabs.create({ url: whatsappURL });
   }
   if (info.menuItemId === "youfone_verlengbaar") {
     const phoneNumber = info.selectionText;
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant,\n\nU heeft via onze winkel een abonnement afgesloten bij Youfone op uw mobiele telefoon. Inmiddels is dit abonnement in de verleng periode gekomen. Wij bieden geen ondersteuning meer voor de providers Youfone en Lebara vanwege hun slechte service. Om verzekerd te blijven van een goede service hebben wij verschillende alternatieven als provider. Hierdoor blijft u tevens verzekerd van de juiste prijs.\nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant,\n\nU heeft via onze winkel een abonnement afgesloten bij Youfone op uw mobiele telefoon. Inmiddels is dit abonnement in de verleng periode gekomen. Wij bieden geen ondersteuning meer voor de providers Youfone en Lebara vanwege hun slechte service. Om verzekerd te blijven van een goede service hebben wij verschillende alternatieven als provider. Hierdoor blijft u tevens verzekerd van de juiste prijs.\nWij nodigen u graag uit in onze winkel. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
     chrome.tabs.create({ url: whatsappURL });
   }
   if (info.menuItemId === "lebara_verlengbaar") {
     const phoneNumber = info.selectionText;
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant,\n\nU heeft via onze winkel een abonnement afgesloten bij Lebara op uw mobiele telefoon. Inmiddels is dit abonnement in de verleng periode gekomen. Wij bieden geen ondersteuning meer voor de providers Youfone en Lebara vanwege hun slechte service. Om verzekerd te blijven van een goede service hebben wij verschillende alternatieven als provider. Hierdoor blijft u tevens verzekerd van de juiste prijs.\nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant,\n\nU heeft via onze winkel een abonnement afgesloten bij Lebara op uw mobiele telefoon. Inmiddels is dit abonnement in de verleng periode gekomen. Wij bieden geen ondersteuning meer voor de providers Youfone en Lebara vanwege hun slechte service. Om verzekerd te blijven van een goede service hebben wij verschillende alternatieven als provider. Hierdoor blijft u tevens verzekerd van de juiste prijs.\nWij nodigen u graag uit in onze winkel. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
     chrome.tabs.create({ url: whatsappURL });
   }
   if (info.menuItemId === "gg_bericht") {
     const phoneNumber = info.selectionText;
     const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
-      `Beste klant,\n\nWij hebben u een aantal keer geprobeerd te bereiken over het mobiele abonnement bij onze winkel die inmiddels in de verlengperiode is gekomen. Graag zouden wij u hierover even spreken om de opties even na te gaan. Dit kan zowel telefonisch als in de winkel. \nWij nodigen u graag uit in onze winkel in Best. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 best@maastelecom.nl. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie Best.`
+      `Beste klant,\n\nWij hebben u een aantal keer geprobeerd te bereiken over het mobiele abonnement bij onze winkel die inmiddels in de verlengperiode is gekomen. Graag zouden wij u hierover even spreken om de opties even na te gaan. Dit kan zowel telefonisch als in de winkel. \nWij nodigen u graag uit in onze winkel in ${locatie}. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
     )}`;
     chrome.tabs.create({ url: whatsappURL });
+  }
+  if (info.menuItemId === "winback"){
+    const phoneNumber = info.selectionText;
+    const whatsappURL = `https://web.whatsapp.com/send?phone=31${phoneNumber}&text=${encodeURIComponent(
+      `Beste klant, \n\n Wij hebben u al een tijdje gemist in onze winkel. In de loop van de tijd is hierdoor het contract uit zijn looptijd gegaan. Dit kan gevolgen hebben voor uw pakket en de bijbehorende prijs van het abonnement. In de afgelopen tijd zijn door diverse providers de prijzen behoorlijk verhoogt door de indexering op de contracten. Hierdoor is het interessant om langs de winkel te komen, om samen te kijken of het lonend is om te verlengen of dat juist overstappen gaat lonen voor u. In alles ontzorgen wij u met deze aanvraag. Dit kan u maandelijks een mooie besparing op leveren. Tevens kunt u dan weer volop genieten van de service en expertise van onze medewerkers in onze winkel. U krijgt dan ook weer gratis ons service pakket gold voor de duur van de looptijd van u contract, helemaal gratis!\n\nWij nodigen u graag uit in onze winkel in ${locatie}. \n\nU kunt ons altijd bereiken via de onderstaande gegevens.\n\n📞 085 111 9393 ${belOptie}\n💻 ${mail}. \nOf plan een afspraak: https://telecombinatie.nl/afspraak \n\nTot ziens 📞📱  \n\nMet vriendelijke groet, \nTeam Maas Telecombinatie ${locatie}.`
+    )}`;
   }
 }
 
@@ -84,15 +93,19 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
   switch (info.menuItemId) {
     case "optie1":
       belOptie = "Optie 1, Best";
+      locatie = "Best";
       break;
     case "optie2":
       belOptie = "Optie 2, Vught";
+      locatie = "Vught";
       break;
     case "optie3":
       belOptie = "Optie 3, Schijndel";
+      locatie = "Schijndel";
       break;
     case "optie4":
       belOptie = "Optie 4, Veldhoven";
+      locatie = "Veldhoven";
       break;
     default:
       break;
@@ -225,4 +238,11 @@ chrome.runtime.onInstalled.addListener(function () {
     id: "outofcontract",
     parentId: "ABP"
   });
+
+  let winback = chrome.contextMenus.create({
+    title: "Winback",
+    contexts: ["selection"],
+    id: "winback",
+    parentId: "ABP"
+  })
 });
